9d14847ebfc0d3b675d86e907761b5129c721332
MamtaY-28 <145557287+MamtaY-28@users.noreply.github.com>
2023-12-12 07:19:56 +0000

    commint for settings

--- HRMSProject/settings.py
+++ HRMSProject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pqgm(eh1^)6j_92)u7+#gpx*=a2&6mb372yf1q49^97bk=66$b'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['x22245855hrmsproject-env.eba-giudykmp.eu-north-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
