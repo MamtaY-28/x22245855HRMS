e97eabe820df36af29b8edccaa80ddf52c9786ea
MamtaY-28 <145557287+MamtaY-28@users.noreply.github.com>
2023-12-12 08:14:36 +0000

    secreate key changes

--- HRMSProject/settings.py
+++ HRMSProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-pqgm(eh1^)6j_92)u7+#gpx*=a2&6mb372yf1q49^97bk=66$b"
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
